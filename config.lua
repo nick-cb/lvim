44533ae5e255164adf577856a7e76f24753e1396
VI Nguyen <nguyenhungvicb@gmail.com>
2022-04-30 23:18:17 +0700

    backup 04-30

--- config.lua
+++ config.lua
@@ -7,6 +7,7 @@ lvim.format_on_save = false
 lvim.colorscheme = "github_dark_default"
 lvim.lsp.diagnostics.virtual_text = false
 lvim.lsp.diagnostics.update_in_insert = false
+lvim.lsp.automatic_servers_installation = false
 
 local formatters = require("lvim.lsp.null-ls.formatters")
 formatters.setup({
@@ -16,17 +17,21 @@ formatters.setup({
 		filetypes = { "typescript", "typescriptreact", "html", "css", "scss", "javascript", "javascriptreact" },
 	},
 })
+-- lvim.lsp.templates_dir = join_paths(get_runtime_dir(), "after", "ftplugin")
 require("lvim.lsp.manager").setup("tailwindcss", {
-	filetypes = { "typescriptreact", "typescript", "html" },
+	filetypes = { "typescriptreact", "typescript", "html", "css", "scss" },
 	root_dir = require("lspconfig").util.root_pattern("tailwind.config.js"),
 })
-require("lvim.lsp.manager").setup("texlab")
 require("lvim.lsp.manager").setup("angularls", {
 	root_dir = require("lspconfig").util.root_pattern("angular.json"),
 })
 require("lvim.lsp.manager").setup("dartls", {
 	root_dir = require("lspconfig").util.root_pattern("pubspec.yaml"),
 })
+-- require("lvim.lsp.manager").setup("cssls", {
+-- 	filetypes = { "css", "scss" },
+-- 	root_dir = require("lspconfig").util.root_pattern(".git"),
+-- })
 
 -- keymappings [view all the defaults by pressing <leader>Lk]
 lvim.leader = "space"
@@ -118,11 +123,65 @@ lvim.builtin.treesitter.ensure_installed = {
 	"rust",
 	"java",
 	"yaml",
+	"html",
+	"scss",
 }
 
 lvim.builtin.treesitter.ignore_install = { "haskell" }
 lvim.builtin.treesitter.highlight.enabled = true
 lvim.builtin.treesitter.autotag = { enable = true }
+lvim.builtin.telescope.defaults.file_ignore_patterns = {
+	"vendor/*",
+	"%.lock",
+	"__pycache__/*",
+	"%.sqlite3",
+	"%.ipynb",
+	"node_modules/*",
+	"%.jpg",
+	"%.jpeg",
+	"%.png",
+	"%.svg",
+	"%.otf",
+	"%.ttf",
+	".git/",
+	"%.webp",
+	".dart_tool/",
+	".github/",
+	".gradle/",
+	".idea/",
+	".settings/",
+	".vscode/",
+	"__pycache__/",
+	"build/",
+	"env/",
+	"gradle/",
+	"node_modules/",
+	"target/",
+	"%.pdb",
+	"%.dll",
+	"%.class",
+	"%.exe",
+	"%.cache",
+	"%.ico",
+	"%.pdf",
+	"%.dylib",
+	"%.jar",
+	"%.docx",
+	"%.met",
+	"smalljre_*/*",
+	".vale/",
+	"%.burp",
+	"%.mp4",
+	"%.mkv",
+	"%.rar",
+	"%.zip",
+	"%.7z",
+	"%.tar",
+	"%.bz2",
+	"%.epub",
+	"%.flac",
+	"%.tar.gz",
+}
 
 -- generic LSP settings
 
@@ -241,16 +300,21 @@ lvim.plugins = {
 		run = { "yarn install --frozen-lockfile && yarn compile" },
 		ft = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
 	},
+	{
+		"Nash0x7E2/awesome-flutter-snippets",
+		ft = { "dart" },
+	},
 	{ "iamcco/markdown-preview.nvim", run = { "cd app && yarn install" }, ft = { "markdown" } },
 	{
 		"projekt0n/github-nvim-theme",
 		config = function()
 			require("github-theme").setup({
 				theme_style = "dark_default",
-        function_style	= "bold"
+				function_style = "bold",
 			})
 		end,
 	},
+	{ "romainl/vim-cool" },
 }
 
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
@@ -262,5 +326,10 @@ require("options")
 require("luasnip.loaders.from_vscode").lazy_load({
 	paths = {
 		"/home/nick/.local/share/lunarvim/site/pack/packer/opt/vscode-es7-javascript-react-snippets/",
+	},
+})
+require("luasnip.loaders.from_vscode").lazy_load({
+	paths = {
+		"/home/nick/.local/share/lunarvim/site/pack/packer/opt/awesome-flutter-snippets/",
 	},
 })
